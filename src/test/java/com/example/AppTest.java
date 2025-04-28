3fa03ec6410274a49259a3d7e936a756f98634d1
rajucharan229@gmail.com <rajucharan229@gmail.com>
2025-04-28 09:52:19 +0530

    my project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+public class AppTest{
+    @Test
+    public void TestAdd() {
+        App app = new App();
+        int result = app.add(2, 3);
+        System.out.println("2+3=" + result);
+        Assert.assertEquals(5, result);
+    }
+}
